9034b5ddeab98d09d4ede34a626894827b697a24
Valeria Vassallo <valevassallo1@gmail.com>
2019-04-29 17:14:09 -0500

    Routes (#21)
    * Init Rails
    
    * Add routes ver. 0

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root "home#index"
+
+  resources :shipments, only: [:show] do
+    get "/search" => :search
+    post "/search" => :search
+
+  resources :users, only: :show
+  resources :senders, only: :show
+
+  namespace :deposit do
+    resources :shipments, only: [:show] do
+      get "/search" => :search
+      post "/search" => :search
+      post "/marked" => :stored
+    end
+  end
+
+  namespace :admin do
+    resources :shipments do
+      get "/sales" => :sales
+    end
+    resources :users, only: :create
+  end
+
+  namespace :api do
+    resources :shipments
+    resources :users
+    resources :senders
+  end
 end
