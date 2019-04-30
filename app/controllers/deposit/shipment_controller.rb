aa197178b0c49c570d1371459f04aad47318295d
Diego Cuevas <diegocuevas579@gmail.com>
2019-04-30 17:18:46 -0500

    Add search api

--- app/controllers/deposit/shipment_controller.rb
+++ app/controllers/deposit/shipment_controller.rb
@@ -2,11 +2,4 @@ class Deposit::ShipmentController < ApplicationController
   def index
   end
 
-  def search
-    if params[:search].present?
-      @shipments = Shipment.where(name: params[:search])
-    else
-      redirect_to "/error"
-    end
-  end
 end
