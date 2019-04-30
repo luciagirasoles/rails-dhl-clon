4a9a6aeeea056033d76914d442b9144b220734b4
mayra navarro <luciagirasoles@gmail.com>
2019-04-29 20:25:44 -0500

    add uniqueness validation on tracking_id

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -2,4 +2,5 @@ class Shipment < ApplicationRecord
   has_many :shipmentlocations, dependent: :destroy
   belongs_to :user, class_name: 'User'
   belongs_to :sender
+  validates :tracking_id, uniqueness: true
 end
