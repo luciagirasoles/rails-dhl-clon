d5ce15ab01f7a8a9a220c9441b5521d57b29d75c
mayra navarro <luciagirasoles@gmail.com>
2019-04-29 20:17:49 -0500

    replace migration

--- db/migrate/20190430011520_add_sender_to_shipment.rb
+++ db/migrate/20190430011520_add_sender_to_shipment.rb
@@ -1,5 +1,5 @@
 class AddSenderToShipment < ActiveRecord::Migration[5.2]
   def change
-    add_reference :shipments, :sender, foreign_key: { to_table: :users }
+    add_reference :shipments, :sender, foreign_key: true
   end
 end
