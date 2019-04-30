aadef731b3f7ed64b4c130f7a41d8d2b2cc5102d
Valeria Vassallo <valevassallo1@gmail.com>
2019-04-29 21:04:18 -0500

    Add Omniauth for Facebook and Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :shipments, dependent: :destroy
+  devise :omniauthable, omniauth_providers: %i[facebook github]
 
+  def self.from_omniauth(auth)
+    user = where(email: auth.info.email).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+    provider = Provider.find_or_create_by(name: auth.provider, uid: auth.uid, user_id: user.id)
+    user
+  end
 end
