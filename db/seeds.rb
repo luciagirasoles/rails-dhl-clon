d7e6023cbab2dd7f409af6f97639bf6004ba7d29
Diego Cuevas <diegocuevas579@gmail.com>
2019-04-30 11:29:51 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 require "faker"
 
-# Create Users
 
 10.times do
   User.create(
@@ -12,40 +11,5 @@ require "faker"
     address: Faker::Address.street_address,
     role: 'regular'
   )
-
-# Creating Sender
-
-5.times do
-  Sender.create ([{
-  store_name: Faker::Commerce.department,
-  email: Faker::Internet.email,
-  order_id: con 
-  }])
-  con += 1
 end
 
-# Create shipment
-
-5.times do
-  Shipment.create ([{
-    tracking_id: Faker::Alphanumeric.alphanumeric(10),
-    origin_adress: Faker::Address.full_address, 
-    destination_address: Faker::Address.full_address, 
-    weight: Faker::Number.between(1, 10),
-    reception_date: Faker::Date.forward(60),
-    estimated_delivery_date: Faker::Date.forward(60),
-    freight_value: Faker::Number.between(20 ,100),
-    user_id: User.all.reduce([]){ |array, val| array << val.id }.sample,
-    sender_id: Sender.all.reduce([]){ |array, val| array << val.id }.sample
-  }])
-# Create Shipment Location
-
-5.times do 
-  ShipmentLocation.create ([{
-    city: Faker::Address.city,
-    country: Faker::Address.country,
-    reception_date:Faker::Date.forward(20),
-    shipment_id: Shipment.all.reduce([]){ |array, val| array << val.id }.sample,
-  }])
-
-end
