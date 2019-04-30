2bb188e41b00c2d73c18321fb25a43333a306a93
mayra navarro <luciagirasoles@gmail.com>
2019-04-30 10:37:51 -0500

    correct user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :omniauthable, omniauth_providers: %i[facebook github]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-  has_many :shipments, dependent: :destroy
-=======
   devise :omniauthable, omniauth_providers: %i[facebook github]
   has_many :shipments, dependent: :destroy
   has_many :providers
->>>>>>> 47689d24052bc3754cb43f25474bca9302130d88
   
   validates :username, presence: true
   validates :username, uniqueness: true
   # validates :country, :city, :address, :reception_date, presence: true
-<<<<<<< HEAD
-
-=======
->>>>>>> 47689d24052bc3754cb43f25474bca9302130d88
 
   def self.from_omniauth(auth)
     user = where(email: auth.info.email).first_or_create do |user|
