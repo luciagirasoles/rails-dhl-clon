43808e0a6f5faa9caba7ad45afae0e7a466271ba
Diego Cuevas <diegocuevas579@gmail.com>
2019-05-01 09:38:38 -0500

    User role Deposit

--- app/controllers/deposit/shipment_controller.rb
+++ app/controllers/deposit/shipment_controller.rb
@@ -3,8 +3,9 @@ class Deposit::ShipmentController < ApplicationController
   end
 
   def search
-    if params[:search].present?
-      @shipments = Shipment.where(name: params[:search])
+    if params[:tracking_id].present?
+      @shipments = Shipment.where(name: params[:tracking_id])
+      redirect_to shipment_path(@shipment)
     else
       redirect_to "/error"
     end
