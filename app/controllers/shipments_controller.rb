64d4d0a9dfcaf333de005c122b96daf369c159c5
mayra navarro <luciagirasoles@gmail.com>
2019-05-01 13:05:27 -0500

    permit search params

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -7,12 +7,17 @@ class ShipmentsController < ApplicationController
   end
 
   def search
-    if params[:tracking_id]
-
-      @shipment = Shipment.find_by(tracking_id: '3nomeuer3h')
+    if params[:search_tracking_id]
+      @shipment = Shipment.find_by(tracking_id: search_param[:search_tracking_id])
       redirect_to shipment_path(@shipment)
     else
-      redirect_to "/error"
+      redirect_to "/error", notice: "Track ID is not found. Please, try again"
     end
   end
+
+  private
+  def search_param
+    params.permit(:search_tracking_id, :utf8)
+  end
+
 end
