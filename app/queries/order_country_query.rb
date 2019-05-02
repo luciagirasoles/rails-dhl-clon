b6818f808e7425bea07a01393d5ddac79a3a077c
Valeria Vassallo <valevassallo1@gmail.com>
2019-05-02 14:41:55 -0500

    Fix detail in queries

--- app/queries/order_country_query.rb
+++ app/queries/order_country_query.rb
@@ -1,9 +1,9 @@
 class OrderCountryQuery
   def top_5_countries_senders
-    Shipment.joins(:sender).group('shipments.id, senders.country').order('count(senders.country) DESC').limit(5).size
+    Shipment.joins(:sender).group('shipments.id, senders.country').order('count(senders.country) DESC').limit(5)
   end
 
   def top_5_countries_recipients
-    Shipment.joins(:recipient).group('shipments.id, users.country').order('count(users.country) DESC').limit(5).size
+    Shipment.joins(:recipient).group('shipments.id, users.country').order('count(users.country) DESC').limit(5)
   end
 end
