866d9d66e83e2dff70eb70318f3261c18f8c702f
Mayra Navarro <luciagirasoles@gmail.com>
2019-05-02 20:57:39 -0500

    Admin panel views - Create new sales User (#62)
    * .gitignore is now working
    
    * generate model
    
    * update devise views
    
    * add more validators
    
    * generate uniqueness field in DB
    
    * replace field type in forms
    
    * add faker gem
    
    * generate User fake data
    
    * correct user model
    
    * remove unnecessary view
    
    * Add more data on seed
    
    * replace seed
    
    * fix seed
    
    * fix seed
    
    * fix relationship
    
    * merge seed
    
    * m
    
    * add nav bar
    
    * Add advances
    
    * Configure route
    
    * Add search
    
    * Delete users_controller.rb
    
    * Delete _first_step.html.erb
    
    * Delete _second_step.html.erb
    
    * update search action
    
    * permit search params
    
    * verify correct id
    
    * correct db:migrate
    
    * correct migrate
    
    * replace params for sanitizer value
    
    * create admin view base
    
    * refactor seed
    
    * fix seed
    
    * duplicate method
    
    * update admin shipments views
    
    * update routes
    
    * correct routes
    
    * define admin and regular views
    
    * update routes
    
    * create index for admin shipment
    
    * create user controller
    
    * define views and controller for admin index
    
    * format sales view
    
    * create sales user
    
    * resolve conflict

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,12 @@ class Admin::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_shipments_path(@user), notice: "The User was successfully created"
+      # redirect_to admin_artist_path(@artist), notice: "The artist was successfully created"
+    else
+      render :new
+    end
   end
   
   def update
