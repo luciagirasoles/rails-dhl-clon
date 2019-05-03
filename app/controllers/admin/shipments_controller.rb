a2b8e0a00b0cf93469efdb122e26498e26e4a0ca
mayra navarro <luciagirasoles@gmail.com>
2019-05-03 14:57:51 -0500

    create search & update shipment views & controller

--- app/controllers/admin/shipments_controller.rb
+++ app/controllers/admin/shipments_controller.rb
@@ -33,14 +33,30 @@ class Admin::ShipmentsController < ApplicationController
   end
 
   def search_and_edit
-  
+
   end
 
+  def search_shipment
+    search_track = Shipment.search(search_param[:search_tracking])
+    if search_track
+      @shipment = search_track
+      redirect_to edit_admin_shipment_path(@shipment)
+    else
+      redirect_to search_and_edit_admin_shipments_path, notice: "Track ID is not found. Please, try again"
+    end
+  end
+  
   def edit
+    @shipment = Shipment.find(params[:id])
   end
 
   def update
-  #update shipment
+    @shipment = Shipment.find(params[:id])
+    if @shipment.update(shipment_params)
+      redirect_to search_and_edit_admin_shipments_path(@shipment), notice: "The Shipment #{@shipment.tracking_id} was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def top_senders_by_freight_value
@@ -62,11 +78,11 @@ class Admin::ShipmentsController < ApplicationController
   private
 
   def search_param
-    params.permit(:search_tracking_id)
+    params.permit(:search_tracking_id, :search_tracking, :utf8)
   end
 
   def shipment_params
-    params.require(:shipment).permit(:tracking_id, :origin_address, :destination_address, :weight, :reception_date, :estimated_delivery_date, :freight_value, :recipient_id, :sender_id)
+    params.require(:shipment).permit(:origin_address, :destination_address, :weight, :reception_date, :estimated_delivery_date, :freight_value, :recipient_id, :sender_id)
   end
 
 end
