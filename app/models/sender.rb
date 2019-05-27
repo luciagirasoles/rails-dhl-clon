fd755202f8e1e1c79533747db08525902de6e357
Mayra Navarro <luciagirasoles@gmail.com>
2019-05-27 00:37:48 -0400

    Update sender.rb

--- app/models/sender.rb
+++ app/models/sender.rb
@@ -1,6 +1,5 @@
 class Sender < ApplicationRecord
-  has_many :shipments
-  has_many :senders, dependent: :destroy
+  has_many :shipments, dependent: :destroy
   validates :order_id, numericality: { greater_than: 0 }
 
 end
